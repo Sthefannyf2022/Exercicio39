125d0d42b71e430e060d6a96f7b83dcf989c6cca
Sthefanny Farias <110677502+Sthefannyf2022@users.noreply.github.com>
2022-11-10 10:45:11 -0300

    Add files via upload

---
+++ 39.py
@@ -0,0 +1,9 @@
+n1= float(input('Digite o 1° valor'))
+n2= float(input('Digite o 2° valor'))
+
+if n1 > n2:
+   print('O 1° valor é maior')
+elif n2 > n1:
+   print('O 2° valor é maior')
+else:
+   print('Os valores são iguais')
